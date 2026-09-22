643d471a1aae56c0cc86c6ebdfbad83ef96181a3
Rashmisingh Bayas <rashmisinghbayas@gmail.com>
2026-09-22 14:59:01 +0300

    Add lab2_1.c

---
+++ src/lab2_1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int sum_to_n(int n) {
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+int main(void) {
+    int n;
+
+    printf("Enter n: ");
+    if (scanf("%d", &n) != 1) {
+        printf("Invalid input.\n");
+        return 1;
+    }
+
+    if (n < 1) {
+        printf("Error: n must be at least 1.\n");
+        return 1;
+    }
+
+    printf("Sum from 1 to %d = %d\n", n, sum_to_n(n));
+    return 0;
+}
