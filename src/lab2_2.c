1318c333f62ab57bf3db0d216c0bb020b8a6aa91
Rashmisingh Bayas <rashmisinghbayas@gmail.com>
2026-09-23 10:23:09 +0300

    Add lab2_2.c

---
+++ src/lab2_2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+long long factorial(int n) {
+    long long result = 1;
+    for (int i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+int main(void) {
+    int n;
+
+    printf("Enter n: ");
+    if (scanf("%d", &n) != 1) {
+        printf("Invalid input.\n");
+        return 1;
+    }
+
+    if (n < 0) {
+        printf("Error: n must be non-negative.\n");
+        return 1;
+    }
+
+    printf("%d! = %lld\n", n, factorial(n));
+    return 0;
+}
